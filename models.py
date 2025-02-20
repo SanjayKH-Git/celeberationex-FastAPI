5420e880d735d035146b6716caf90a7a1c2a2ddb
SanjayKH-Git <56336350+SanjayKH-Git@users.noreply.github.com>
2025-02-21 02:02:59 +0530

    ENV update (#2)
    * vendor_categories & celeberation_list API
    
    * celeberation_list & vendor_categories API
    
    * ENVIRONMENT VARIABLE Update

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, JSON
 from database import Base
 
 class VendorCategory(Base):
     __tablename__ = "vendor_categories"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
+    details = Column(JSON)  # JSON type for details
+    image_url = Column(String)  # Add this line
 
 class CeleberationList(Base):
     __tablename__ = "celeberation_list"
